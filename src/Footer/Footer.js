06175ab68f9a55e4c7d9b72bea0278b901483d82
Ion901 <ionerhan13@gmail.com>
2023-03-17 18:32:51 +0200

    to many changes to explain

---
+++ src/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../index.css';
+
+function Footer(){
+    return(
+        <div className="--footer">
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-square-instagram"></i>
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-linkedin"></i>
+        </div>
+    )
+}
+
+export default Footer;
